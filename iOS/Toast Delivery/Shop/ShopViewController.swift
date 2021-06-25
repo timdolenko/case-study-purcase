64d16d2d531159148f64c4752c83bc43f9fe0814
Tim <sispo777@gmail.com>
2021-06-25 09:08:30 +0200

    Added bindings on ShopVC

--- iOS/Toast Delivery/Shop/ShopViewController.swift
+++ iOS/Toast Delivery/Shop/ShopViewController.swift
@@ -4,23 +4,46 @@
 //
 
 import UIKit
+import Combine
 
 // TODO: create a checkout with shopping cart item
 class ShopViewController: UIViewController {
+    
+    var viewModel = ShopViewModel()
+    weak var listVC: ToastListViewController!
+    weak var cartVC: CartViewController!
+    private var bindings = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupListView()
+        setupCartView()
+        setupConstraints()
+
+        listVC.didMove(toParent: self)
+        cartVC.didMove(toParent: self)
+        
+        bind()
+    }
+    
+    private func setupListView() {
         let listVC = createToastList()!
         listVC.view.translatesAutoresizingMaskIntoConstraints = false
         addChild(listVC)
         view.addSubview(listVC.view)
-
+        self.listVC = listVC
+    }
+    
+    private func setupCartView() {
         let cartVC = CartViewController()
         cartVC.view.translatesAutoresizingMaskIntoConstraints = false
         addChild(cartVC)
         view.addSubview(cartVC.view)
-
+        self.cartVC = cartVC
+    }
+    
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
             listVC.view.topAnchor.constraint(equalTo: view.topAnchor),
             listVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -31,9 +54,28 @@ class ShopViewController: UIViewController {
             cartVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             cartVC.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
-
-        listVC.didMove(toParent: self)
-        cartVC.didMove(toParent: self)
+    }
+    
+    private func bind() {
+        bindListView()
+        bindCartView()
+    }
+    
+    private func bindListView() {
+        let viewModel = ToastListViewModel()
+        viewModel
+            .$selectedToast
+            .assign(to: \.selectedToast, on: self.viewModel)
+            .store(in: &bindings)
+        
+        listVC.viewModel = ToastListViewModel()
+    }
+    
+    private func bindCartView() {
+        viewModel
+            .$selectedToast
+            .assign(to: \.toast, on: cartVC)
+            .store(in: &bindings)
     }
 
     private func createToastList() -> ToastListViewController? {
